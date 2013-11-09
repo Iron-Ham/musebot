f579361f2e903076b5d2100a0c337f88780399a5
beshoy hanna <bigshot929@yahoo.com>
2013-11-09 13:48:06 -0500

    Actual GUI
    Hesham/Michael - we may need to scale this image from 720P LOL. Lets
    talk Monday.

--- MuzeBot/src/Selection.java
+++ MuzeBot/src/Selection.java
@@ -1,11 +1,14 @@
+import java.io.IOException;
+
 
 public class Selection {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		splashFrame splashSplash = new splashFrame();
+		splashSplash.show(true);
 
 	}
 
