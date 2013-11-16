91409d94756aea0df18decd0f96951c5da9077fb
beshoy hanna <bigshot929@yahoo.com>
2013-11-15 19:23:34 -0500

    Quiz also now contains testing components
    YEEEEEEEEEEEEHAW!

--- MuzeBot/src/Practice.java
+++ MuzeBot/src/Practice.java
@@ -1,26 +1,3 @@
-//import java.awt.BorderLayout;
-//
-//import javax.swing.JFrame;
-//
-//
-//public class Practice extends JFrame{
-//
-//	
-//	public Practice() {
-//		setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		setSize(800,600);
-//		
-//		SheetMusic sm = new SheetMusic("4/4");
-//		sm = sm.generateRandomSheetMusic(2);
-//		SheetMusicComponent smc = new SheetMusicComponent(sm, 0, 0, 500, 200, this);
-//		setLayout(new BorderLayout());
-//		add(smc);
-//		
-//	}
-//}
-
-
-
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
