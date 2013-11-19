0e5a8a568623370a45cf70d168d74e6274ea572a
Hesham Salman <Hesham@Heshams-MacBook-Pro.local>
2013-11-18 20:12:04 -0500

    Added PDF Library

--- MuseBot/src/Lesson.java
+++ MuseBot/src/Lesson.java
@@ -1,6 +1,33 @@
-import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.*;
+import java.io.*;
+
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
 
 
 public class Lesson extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -359398248582066716L;
+	private Image image;
+	private JWebBrowser fileBrowser;	//Use a web component to display the PDF
+	public Lesson() { 
+		try{
+			this.image = ImageIO.read(new File("Statics/Lessons/Lesson 1.jpg"));	//This needs to change later.
+			
+		} catch (IOException ex) { 
+			ex.printStackTrace();
+		}
+		fileBrowser = new JWebBrowser(); 
+		fileBrowser.setBarsVisible(false);
+		fileBrowser.setStatusBarVisible(false);
+		setSize(1280, 720);
+		setTitle("Lesson");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+	}
 }
