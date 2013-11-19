f32b8905f3d686cc59819a900b70b60ea41e55f6
Hesham Salman <Hesham@Heshams-MacBook-Pro.local>
2013-11-18 20:17:46 -0500

    PDFViewer Panel Complete
    I finished up the PDFViewer panel. All this panel does is display a PDF
    in a web component inside the panel if you supply it with the absolute
    filepath.

--- MuseBot/src/LessonViewer.java
+++ MuseBot/src/LessonViewer.java
@@ -7,27 +7,26 @@ import java.io.*;
 import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
 
 
-public class Lesson extends JFrame {
+public class LessonViewer extends JPanel {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -359398248582066716L;
 	private Image image;
+	String filePath;
 	private JWebBrowser fileBrowser;	//Use a web component to display the PDF
-	public Lesson() { 
-		try{
-			this.image = ImageIO.read(new File("Statics/Lessons/Lesson 1.jpg"));	//This needs to change later.
-			
-		} catch (IOException ex) { 
-			ex.printStackTrace();
-		}
+	public LessonViewer(String path) { 
+		filePath = path; 			//You need absolute filepath!
 		fileBrowser = new JWebBrowser(); 
 		fileBrowser.setBarsVisible(false);
 		fileBrowser.setStatusBarVisible(false);
 		setSize(1280, 720);
-		setTitle("Lesson");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
+		add(fileBrowser, BorderLayout.CENTER);
+		fileBrowser.navigate(filePath);
+	}
+	public void setPDFFilePath(String path) {
+		filePath = path;
 	}
 }
