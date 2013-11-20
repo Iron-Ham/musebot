99b34738c85653ec55c80d64ba0491fcc8f7592d
beshoy hanna <bigshot929@yahoo.com>
2013-11-19 21:04:36 -0500

    blah

--- MuzeBot/src/ResultsTracker.java
+++ MuzeBot/src/ResultsTracker.java
@@ -1,22 +1,48 @@
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class ResultsTracker {
+List QuizList = new LinkedList();
 
-	public ResultsTracker(String filename) throws IOException{
+	public boolean ResultsTracker(String filename) throws IOException{
 		
 		File database = new File(filename);
 		
 		if(database.exists()){
+			
 			  Scanner in = new Scanner(database).useDelimiter("[\t|\n|\r]+");
 			  while (in.hasNext()){
-				  
+				QuizList.add(in.nextLine()); 
 			  }
+			  
+			  return true;
 		  }else{
 			  database.createNewFile();
+			  
+			  return false;
 		  }
 		
+	}
+	
+	public double GetAverageScoresOfQuizByNumber(int Number){
+		
+		
+		return 0.00;
+		
+	}
+	
+public List GetHighLowScoresOfQuizByNumber(int Number){
+	
+	double QL = 0.00;
+	double QH = 0.00;
+	/////////////////
+	List<Double> a = new LinkedList<Double>();
+	a.add(QL);
+	a.add(QH);
+	
+	return a;	
 	}
 	
 }
