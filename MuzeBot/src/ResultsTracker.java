7393d1a9ffcfcddec27ffe7e8d2fa874eb51f2cc
beshoy hanna <bigshot929@yahoo.com>
2013-11-19 22:00:12 -0500

    update2

--- MuzeBot/src/ResultsTracker.java
+++ MuzeBot/src/ResultsTracker.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.*;
@@ -48,7 +50,7 @@ List QuizList = new LinkedList();
 		
 	}
 	
-public List GetHighLowScoresOfQuizByNumber(int Number){
+public List GetHighScoreOfQuizesByNumber(int Number){
 	
 	double QL = 0.00;
 	double QH = 0.00;
@@ -57,7 +59,44 @@ public List GetHighLowScoresOfQuizByNumber(int Number){
 	a.add(QL);
 	a.add(QH);
 	
-	return a;	
+	for (int i = 0; i<QuizList.size(); i++){
+		String HighlightQuiz = QuizList.get(i).toString();
+		
+		if (Integer.valueOf(HighlightQuiz.charAt(0)) == Number){
+			
+		}
 	}
 	
+	return a;	
+	}
+
+public boolean AddScoreOfQuiz(int Number, double Score, String filename) throws IOException{
+	File database = new File(filename);
+	if(database.exists()){
+		
+		  FileWriter fstream = new FileWriter(filename);
+		  BufferedWriter out = new BufferedWriter(fstream);
+		  for (int i = 0; i<QuizList.size(); i++){
+			  out.write(QuizList.get(i).toString()); 
+			  out.newLine();
+		  }
+		  QuizList.add(Number + "\t" +Score);
+		  out.write(Number + "\t" +Score);
+		  out.close();
+		  
+		  return true;
+	  }else{
+		  FileWriter fstream = new FileWriter(filename);
+		  BufferedWriter out = new BufferedWriter(fstream);
+		  out.write(Number + "\t" +Score);
+		  out.close();
+		  return false;
+	  }
+	
 }
+	
+}
+
+
+
+
